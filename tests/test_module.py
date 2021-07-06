1e4a0ba0096f6b19c9d405725a44afd41ef8857e
Tyler Battle <tyler@battle.ca>
2021-07-06 13:13:07 -0700

    Initial run of rename script

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,8 +1,8 @@
 import unittest
 
-import stactools.package
+import stactools.nalcms
 
 
 class TestModule(unittest.TestCase):
     def test_version(self):
-        self.assertIsNotNone(stactools.package.__version__)
+        self.assertIsNotNone(stactools.nalcms.__version__)
