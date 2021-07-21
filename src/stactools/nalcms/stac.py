8f029a892cacf346f8dc423d98b19358a29ef888
James Townend <jvrt@pm.me>
2021-07-20 17:34:07 -0700

    Basic STAC structure in place

--- src/stactools/nalcms/stac.py
+++ src/stactools/nalcms/stac.py
@@ -3,17 +3,37 @@ from dateutil.relativedelta import relativedelta
 import pytz
 # import json
 import logging
-from stactools.nalcms.constants import (ID, TITLE, DESCRIPTION, PROVIDER,
-                                        LICENSE)
+from stactools.nalcms.constants import (
+    EXTENTS, ID, KEYWORDS, LICENSE_LINK, PROJECTIONS, SATELLITES, TITLE,
+    DESCRIPTION, PROVIDER, LICENSE, GSDS, YEARS, REGIONS, HREF_DIR, HREFS_ZIP,
+    HREFS_METADATA, nalcms_catalog, nalcms_license, gsd30m_collection,
+    gsd250m_collection)
 import re
 import shapely.geometry
 import pystac
+from datetime import datetime
+import fiona
+from pyproj import crs, Transformer
+from pystac import (Catalog, Collection, StacIO, Asset, Item, MediaType,
+                    SpatialExtent, Link, Summaries)
+from pystac.extensions.projection import (SummariesProjectionExtension,
+                                          ProjectionExtension)
+from shapely.geometry import box
+from shapely.ops import transform as shapely_transform
+from stactools.nrcan_spot_ortho.geobase_ftp import GeobaseSpotFTP
+from stactools.nrcan_spot_ortho.utils import (bbox, transform_geom,
+                                              CustomStacIO)
+from stactools.nrcan_spot_ortho.stac_templates import (spot_sensor, proj_epsg)
+import os
+
+StacIO.set_default(CustomStacIO)
+null = None
 
 logger = logging.getLogger(__name__)
 
 
 def create_item(cog_href: str) -> pystac.Item:
-    """Creates a STAC item for a "North American Land Change Monitoring System dataset.
+    """Creates a STAC item for a North American Land Change Monitoring System dataset.
 
     Args:
         cog_href (str, optional): Path to COG asset.
@@ -114,3 +134,135 @@ def create_collection(metadata: dict):
     # collection.add_link(LICENSE_LINK)
 
     return collection
+
+
+def create_item(reg, gsd, year):
+    """
+    TODO
+    """
+    bbox = EXTENTS[reg]
+    polygon = shapely.geometry.box(*bbox, ccw=True)
+    coordinates = [list(i) for i in list(polygon.exterior.coords)]
+    geometry = {"type": "Polygon", "coordinates": [coordinates]}
+
+    years = year.split("-")
+    diff = "change " if "-" in year else ""
+    properties = {
+        "title": f"{reg} land cover {diff}({year}, {gsd} m)",
+        "description":
+        f"Land cover {diff}for {year} over {REGIONS[reg]} ({gsd} m)",
+        "start_datetime": datetime.strptime(years[0], "%Y"),
+        "end_datetime": datetime.strptime(years[-1], "%Y"),
+        "gsd": gsd,
+    }
+
+    # Create item
+    item = pystac.Item(
+        id=f"{reg}_{year}_{gsd}m",
+        geometry=geometry,
+        bbox=bbox,
+        datetime=datetime.strptime(years[0], "%Y"),
+        properties=properties,
+        stac_extensions=[
+            "https://stac-extensions.github.io/projection/v1.0.0/schema.json"
+        ])
+
+    # Create metadata asset
+    metadata_href = os.path.join(HREF_DIR, HREFS_METADATA[f"{gsd}m_{year}"])
+    item.add_asset(
+        "metadata",
+        pystac.Asset(
+            href=metadata_href,
+            media_type=MediaType.JSON,
+            roles=["metadata"],
+            title="Metadata for land cover {diff}for {year} ({gsd} m)",
+        ))
+
+    # Create source data asset
+    data_href = os.path.join(HREF_DIR, HREFS_ZIP[f"{gsd}m_{year}_{reg}"])
+    item.add_asset(
+        "data",
+        pystac.Asset(
+            href=data_href,
+            media_type="application/zip",
+            roles=["data"],
+            title=
+            "Data for land cover {diff}over {REGIONS[reg]} for {year} ({gsd} m)"
+        ))
+
+    # Include projection information
+    proj_key = f"{gsd}m_{year}_{reg}"
+    proj_ext = ProjectionExtension.ext(item)
+    proj_ext.epsg = PROJECTIONS[proj_key]["epsg"]
+    proj_ext.transform = PROJECTIONS[proj_key]["transform"]
+    proj_ext.bbox = PROJECTIONS[proj_key]["bounds"]
+    proj_ext.wkt2 = PROJECTIONS[proj_key]["wkt"]
+
+    return item
+
+
+def create_region_collection(reg):
+    """
+    TODO
+    """
+    region = Collection(
+        id=f"NALCMS_{reg}",
+        description=f"Land classification for {REGIONS[reg]}",
+        extent=EXTENTS[reg],
+        title=f"NALCMS for {REGIONS[reg]}",
+        stac_extensions=None,
+        license=LICENSE,
+        summaries=Summaries({
+            "platform":
+            sum([v["platform"] for v in SATELLITES.values()], []),
+            "instruments":
+            sum([v["instruments"] for v in SATELLITES.values()], []),
+            "constellation":
+            sum([v["constellation"] for v in SATELLITES.values()], []),
+            "gsd":
+            GSDS,
+        }))
+
+    return region
+
+
+def create_nalcms_collection():
+    """
+    TODO
+    """
+    nalcms = Collection(
+        id=ID,
+        description=DESCRIPTION,
+        extent=EXTENTS["NA"],
+        title=TITLE,
+        stac_extensions=[
+            "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
+        ],
+        license=LICENSE,
+        keywords=KEYWORDS,
+        providers=PROVIDER,
+        summaries=Summaries({
+            "platform":
+            sum([v["platform"] for v in SATELLITES.values()], []),
+            "instruments":
+            sum([v["instruments"] for v in SATELLITES.values()], []),
+            "constellation":
+            sum([v["constellation"] for v in SATELLITES.values()], []),
+            "gsd":
+            GSDS,
+        }))
+    proj_ext = SummariesProjectionExtension(nalcms)
+    proj_ext.epsg = list(PROJECTIONS.values())
+    nalcms.add_link(LICENSE_LINK)
+
+    for reg in REGIONS.keys():
+        region = create_region_collection(reg)
+        nalcms.add_child(region)
+
+        for gsd, year in [(g, y) for g in GSDS for y in YEARS]:
+            if (gsd == 250) and (reg != "NA"):
+                continue
+            item = create_item(reg, gsd, year)
+            region.add_child(item)
+
+    return nalcms
---
+++ src/stactools/nalcms/utils.py
@@ -0,0 +1,137 @@
+from ftplib import error_perm
+import os
+from urllib.parse import urlparse
+from pystac import Link
+from pystac.stac_io import DefaultStacIO
+from typing import Union, Any
+import zipfile
+import logging
+from subprocess import Popen, PIPE, STDOUT
+import boto3
+from botocore.errorfactory import ClientError
+
+logger = logging.getLogger(__name__)
+
+
+class CustomStacIO(DefaultStacIO):
+    def __init__(self):
+        self.s3 = boto3.resource("s3")
+
+    def read_text(self, source: Union[str, Link], *args: Any,
+                  **kwargs: Any) -> str:
+        parsed = urlparse(source)
+        if parsed.scheme == "s3":
+            bucket = parsed.netloc
+            key = parsed.path[1:]
+
+            obj = self.s3.Object(bucket, key)
+            return obj.get()["Body"].read().decode("utf-8")
+        else:
+            return super().read_text(source, *args, **kwargs)
+
+    def write_text(self, dest: Union[str, Link], txt: str, *args: Any,
+                   **kwargs: Any) -> None:
+        parsed = urlparse(dest)
+        if parsed.scheme == "s3":
+            bucket = parsed.netloc
+            key = parsed.path[1:]
+            s3 = boto3.resource("s3")
+            s3.Object(bucket, key).put(Body=txt, ContentEncoding="utf-8")
+        else:
+            super().write_text(dest, txt, *args, **kwargs)
+
+
+def call(command):
+    def log_subprocess_output(pipe):
+        for line in iter(pipe.readline, b''):  # b'\n'-separated lines
+            logger.info(line.decode("utf-8").strip('\n'))
+
+    process = Popen(command, stdout=PIPE, stderr=STDOUT)
+    with process.stdout:
+        log_subprocess_output(process.stdout)
+    return process.wait()  # 0 means success
+
+
+def upload_to_s3(parsed, local_path):
+    bucket = parsed.netloc
+    key = parsed.path[1:]
+    s3 = boto3.resource("s3")
+    print(f"Uploading {os.path.basename(local_path)}")
+    s3.Bucket(bucket).upload_file(local_path, key)
+
+
+def file_exists(path):
+    parsed = urlparse(path)
+
+    if parsed.scheme == "s3":
+        bucket = parsed.netloc
+        key = parsed.path[1:]
+        s3 = boto3.client('s3')
+        try:
+            s3.head_object(Bucket=bucket, Key=key)
+            return True
+        except ClientError:
+            # Not found
+            return False
+
+    else:
+        return os.path.exists(path)
+
+
+def download_from_ftp(href, out_path, ftp):
+    path = href.split(ftp.ftp_site)[-1]
+    print(f"Downloading {os.path.basename(path)}")
+    with open(out_path, 'wb') as f:
+        try:
+            ftp.ftp.retrbinary(f"RETR {path}", f.write)
+            return True
+        except error_perm:
+            print(f"Failed to open {path} on FTP")
+            return False
+
+
+def unzip(zip_path, out_folder):
+    zfile = zipfile.ZipFile(zip_path, 'r')
+    out_paths = []
+
+    for zip_file in [f for f in zfile.namelist() if '.tif' in f]:
+        (folder, filename) = os.path.split(zip_file)
+        out_path = os.path.join(out_folder, filename)
+        out_paths.append(out_path)
+
+        print(f"Decompressing {folder}{filename}")
+        with open(out_path, 'wb') as f:
+            f.write(zfile.read(zip_file))
+
+    return out_paths
+
+
+def bbox(f):
+    x, y = zip(*list(explode(f["geometry"]["coordinates"])))
+    return min(x), min(y), max(x), max(y)
+
+
+def transform_geom(transformer, geom):
+    """
+    Transform the geometry of a given feature
+    Allow multipolygons
+    """
+    new_coords = []
+    for ring in geom:
+        x2, y2 = transformer.transform(*zip(*ring))
+        new_coords.append(list(zip(y2, x2)))
+
+    return new_coords
+
+
+def explode(coords):
+    # from https://gis.stackexchange.com/questions/90553/fiona-get-each-feature-extent-bounds
+    """Explode a GeoJSON geometry's coordinates object and yield coordinate tuples.
+    As long as the input is conforming, the type of the geometry doesn't matter."""
+    for e in coords:
+        if isinstance(e, (float, int)):
+            yield coords
+            break
+        else:
+            for f in explode(e):
+                yield f
