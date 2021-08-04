b0866b55da275c060155c81cfd019c62760a3c48
James Townend <jvrt@pm.me>
2021-08-04 10:46:01 -0700

    Created tests

---
+++ tests/test_stac.py
@@ -0,0 +1,30 @@
+import unittest
+
+from stactools.nalcms.stac import create_nalcms_collection, create_item
+
+from tests import test_data
+
+
+class TestSTAC(unittest.TestCase):
+    def test_create_item(self):
+        args = {
+            "reg": "NA",
+            "gsd": 30,
+            "year": "2010-2015",
+            "source": "cog_filename.tif"
+        }
+
+        item = create_item(**args)
+
+        self.assertEqual(item.id, 
+        f"{args['reg']}_{args['year']}_{args['gsd']}m")
+
+        for asset in item.assets.values():
+            self.assertIn(args["source"], asset.href)
+
+        item.validate()
+
+    def test_create_collection(self):
+        collection = create_nalcms_collection()
+        collection.set_root(None)
+        collection.validate()
